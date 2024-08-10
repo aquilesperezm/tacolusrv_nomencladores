f7c968866dc422c7b00bb34c782f11c5cf034998
Aquiles Perez Miranda <98031710+aquilesperezm@users.noreply.github.com>
2024-08-09 21:22:28 -0400

    Tacografos fields added: fechas y comentarios

--- Model/Tacografo.php
+++ Model/Tacografo.php
@@ -16,30 +16,44 @@ class Tacografo extends ModelClass
 
     /** @var string */
     public $numero_serie;
-    
+
     /** @var int */
     public $id_modelo;
 
     /** @var string */
     public $id_categoria;
 
+    /** @var int */
+    public $escala_velocidad;
+
+    /** @var string */
+    public $homologacion;
+
+    /** @var int */
+    public $id_vehiculo;
+
     /** @var string */
     public $fecha_fabricacion;
 
-     /** @var int */
-     public $escala_velocidad;
+    /** @var string */
+    public $fecha_instalacion;
 
-     /** @var string */
-    public $homologacion;
+    /** @var string */
+    public $fecha_ultima_revision;
 
-     /** @var int */
-     public $id_vehiculo;
+    /** @var string */
+    public $fecha_fin_garantia;
 
-    
+    /** @var string */
+    public $comentarios;
+
     public function clear()
     {
         parent::clear();
         $this->fecha_fabricacion = date(self::DATE_STYLE);
+        $this->fecha_instalacion = date(self::DATE_STYLE);
+        $this->fecha_ultima_revision = date(self::DATE_STYLE);
+        $this->fecha_fin_garantia = date(self::DATE_STYLE);
     }
 
     public static function primaryColumn(): string
